7ff2da45cd76c18b359b098f73fc65d6ca8a0a36
LeekFoo <etaaniajp@infoseek.jp>
2019-11-25 04:46:51 +0000

    テーマ作成に必要なファイルを追加

---
+++ public_html/wp-content/themes/leekfoo_portfolio/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<meta name="viewport"
+content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/reset.css">
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous"><!--font-awesomeのスタイルシートの呼び出し-->
+
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<header>
+  <div class="header-inner">
+
+  </div><!--end header-inner-->
+</header>
---
+++ public_html/wp-content/themes/leekfoo_portfolio/index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<div class="container">
+  <div class="contents">
+    <div><?php echo get_stylesheet_uri(); ?></div>
+  </div><!--end contents-->
+  <?php get_sidebar(); ?>
+</div><!--end container-->
+<?php get_footer(); ?>
