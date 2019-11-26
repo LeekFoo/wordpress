cedd2d4ab5ae27ed6fc172ed9541b29567341ca8
LeekFoo <etaaniajp@infoseek.jp>
2019-11-26 03:00:24 +0000

    途中コミット

--- public_html/wp-content/themes/leekfoo_portfolio/index.php
+++ public_html/wp-content/themes/leekfoo_portfolio/index.php
@@ -1,6 +1,9 @@
 <?php get_header(); ?>
 <div class="container">
   <div class="contents">
+    <?php if(have_posts()): while(have_posts()): the_post(); ?>
+        <?php get_template_part('loop-content'); ?>
+    <?php endwhile; endif; ?>
   </div><!--end contents-->
   <?php get_sidebar(); ?>
 </div><!--end container-->
