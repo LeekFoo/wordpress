eb8b28e1f54a85c3e2434c8faa760445c80edb6e
LeekFoo <etaaniajp@infoseek.jp>
2019-11-26 03:06:13 +0000

    ページネーション作成

--- public_html/wp-content/themes/leekfoo_portfolio/index.php
+++ public_html/wp-content/themes/leekfoo_portfolio/index.php
@@ -4,6 +4,14 @@
     <?php if(have_posts()): while(have_posts()): the_post(); ?>
         <?php get_template_part('loop-content'); ?>
     <?php endwhile; endif; ?>
+    <div class="pagination">
+        <?php echo paginate_links( array(
+            'type' => 'list',
+            'mid_size' => '1',
+            'prev_text' => '<i class="fas fa-angle-left"></i>',
+            'next_text' => '<i class="fas fa-angle-right"></i>'
+        ) ); ?>
+    </div>
   </div><!--end contents-->
   <?php get_sidebar(); ?>
 </div><!--end container-->
